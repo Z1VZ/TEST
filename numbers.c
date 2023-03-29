9e6e9d76abfa9c40c7c4897da4b43f64eb51dec6
Sivsanker  Ezhuthachan <20716895@vdi-1804-cs-034.staff.ad.curtin.edu.au>
2023-03-29 12:30:30 +0800

    added print statemnet

---
+++ numbers.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "order.h"
+
+int main(){
+	printf("hello");
+	int choice1;
+	int* ptr1 = (int*) malloc(sizeof(int));
+	int* ptr2  = (int*) malloc(sizeof(int));
+	int* ptr3 = (int*) malloc(sizeof(int));
+	printf("Input 3 integers for sorting");
+	printf("num 1: ");
+	scanf("%d", ptr1);
+	printf("num 2: ");
+	scanf("%d", ptr2);
+	printf("num 3: ");
+	scanf("%d", ptr3);
+
+	printf("AScending(1) or Descending(2)?");
+	scanf("%d",&choice1);
+	
+	if(choice1 == 1){
+		ascending3(ptr1, ptr2, ptr3);
+			
+	} else if(choice1 == 2){
+		descending3(ptr1, ptr2, ptr3);
+	
+	}
+
+	free(ptr1);
+	free(ptr2);
+	free(ptr3);
+	return 0;
+}
+
+
